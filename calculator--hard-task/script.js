69d6333aaf51f4b8aa6b97a12adc3e87ab9ea06c
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-02 18:48:31 +0300

    lesson02 done with hard--task

--- calculator--hard-task/script.js
+++ calculator--hard-task/script.js
@@ -1,37 +1,4 @@
-// Решение 1
-
-// let num = 266219,
-//     numToString = num.toString(),
-//     arr = numToString.split('');
-
-// let resulp = arr.reduce(function(a, b) {
-//      return a * b;
-//  });
-
-//  console.log(result);
-
-// console.log(+result.toString().slice(0, 2));
-
-
-// Решение 2
-
-// let num = 266219,
-//     numToString = num.toString(),
-//     arr = numToString.split(''),
-//     result = 1;
-
-// arr.forEach(item => {
-//     result *= item;
-// })
-
-// console.log(result);
-
-// result **= 3;
-
-// console.log(+result.toString().slice(0, 2));
-
-
-// Решение 3
+// Решение 1 c помощью циклов for
 
 let num = 266219,
     numToString = num.toString(),
@@ -49,3 +16,35 @@ for(let i = 0; i < arr.length; i++) {
 console.log(result);
 
 console.log(+result.toString().slice(0, 2));
+
+
+// Решение 2 с помощью split() и forEach 
+
+// let num = 266219,
+//     arr = num.toString().split(''),
+//     result = 1;
+
+// arr.forEach(item => {
+//     result *= item;
+// })
+
+// console.log(result);
+
+// result **= 3;
+
+// console.log(+result.toString().slice(0, 2));
+
+
+// Решение 3 с помощью split() и reduse()
+
+// let num = 266219,
+//     arr = num.toString().split('');
+
+// let result = arr.reduce(function(a, b) {
+//      return a * b;
+//  });
+
+//  console.log(result);
+
+// console.log(+result.toString().slice(0, 2));
+
