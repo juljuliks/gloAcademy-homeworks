16cb9e43bcd5291f2fc6ef3652969855ac31775e
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-02 21:10:05 +0300

    lesson02 done with hard--task

--- calculator--hard-task/script.js
+++ calculator--hard-task/script.js
@@ -2,7 +2,7 @@
 
 let num = 266219,
     numToString = num.toString(),
-    arr = numToString.split('');
+    arr = [],
     result = 1;
 
 for(let i = 0; i < numToString.length; i++) {
@@ -14,6 +14,8 @@ for(let i = 0; i < arr.length; i++) {
 } 
 
 console.log(result);
+
+result **= 3;
 
 console.log(+result.toString().slice(0, 2));
 
