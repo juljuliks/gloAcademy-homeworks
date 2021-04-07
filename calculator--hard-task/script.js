83170cc1ed30299682f997f3cd21aa091f842ddd
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-07 22:30:15 +0300

    fix getTrimmed function

--- calculator--hard-task/script.js
+++ calculator--hard-task/script.js
@@ -1,16 +1,17 @@
 // Задание 1
 
 let getTrimmed = function(str) {
-    str = str.trim();
     if (typeof str !== 'string') {
         console.log('В качестве аргумента должна быть строка!');
         return;
     } else if (str.length > 30) {
-        return str.slice(0, 30) + '...';
+        return str.trim().slice(0, 30) + '...';
+    } else {
+        return str.trim();
     }
-    return str;
+    
 }
 
-let result = getTrimmed('    Lorem                    ')
+let result = getTrimmed('     rewh hwteh wth wth wt wth w strjrjwr         ')
 console.log(result);
 
