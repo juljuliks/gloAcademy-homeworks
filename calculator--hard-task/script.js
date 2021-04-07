ba316d969cf05caa0e0250d029e9f705f2bd4d60
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-07 19:51:44 +0300

    lesson05 done with hard-task

--- calculator--hard-task/script.js
+++ calculator--hard-task/script.js
@@ -7,19 +7,6 @@ arr.forEach(item => {
     }
 })
 
-// Задание 2 решение с помощью меток
-let num = 100;
-
-getNextNumber:
-for(let i = 1; i <= num; i++) {
-    if (i === 1) continue;
-   
-    for(let n = 1; n < i; n++) {
-        if (i % n === 0) continue getNextNumber;
-    }
-
-    console.log(`Делители числа ${i}: 1 и ${i}`);
-}
 
 // Решение с помощью функции и цикла
 
@@ -34,3 +21,19 @@ function getPrimeDigits(num) {
 }
 
 getPrimeDigits(100);
+
+
+// Задание 2 решение с помощью меток
+
+let num = 100;
+
+getNextNumber:
+for(let i = 1; i <= num; i++) {
+    if (i === 1) continue;
+
+    for(let n = 1; n < i; n++) {
+        if (n === 1) continue;
+        if (i % n === 0) continue getNextNumber;
+    }
+    console.log(`Делители числа ${i}: 1 и ${i}`);
+}
