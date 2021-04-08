3c1f91adc578d5d5af1edb0fc25078c751e679c0
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-08 16:56:24 +0300

    getRandomNumber

--- guessRandomNumber/script.js
+++ guessRandomNumber/script.js
@@ -1,14 +1,42 @@
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
+function guessRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 100 + 1);
+    let counter = 0;
+    let maxTries = 10;
+    // console.log('Загаданное число ',randomNumber);
 
-let userAnswer;
+    return function() {
+        if (counter === 10) {
+            console.log('Попытки закончились, хотите сыграть еще?');
+            return;
+        }
 
-function getRandomNum() {
-    Math.round((Math.random() * 100 + 1));
+        let userGuess = prompt("Угадай число от 1 до 100");
+        // console.log('Догадка пользователя ',userGuess);
+
+        if (isNaN(userGuess) || userGuess.trim() === '') {
+            console.log('Введите число!');
+            control();
+        } 
+
+        else if (userGuess === null) {
+            console.log('До свидания!');
+            return;
+        }
+        else if (userGuess > randomNumber) {
+            counter++;
+            console.log(`Загаданное число меньше, осталось попыток ${maxTries - counter}`);
+            control();
+        } 
+        else if (userGuess < randomNumber) {
+            counter++;
+            console.log(`Загаданное число больше, осталось попыток ${maxTries - counter}`);
+            control();
+        } 
+        else if (userGuess === randomNumber) {
+            console.log('Поздравляю, Вы угадали!!!');
+        }
+    }
 }
 
-let start = function() {
-    userAnswer = prompt('Угадай число от 1 до 100')
-}
-
+const control = guessRandomNumber();
+control();
