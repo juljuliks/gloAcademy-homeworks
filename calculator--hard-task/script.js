5a8887d29af4d0967ec939af6a74937832aa8f38
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-10 18:06:12 +0300

    lesson07 hard-task done

--- calculator--hard-task/script.js
+++ calculator--hard-task/script.js
@@ -1,39 +1,13 @@
-// Задание 1
-let arr = [24547, 3569, 4789, 213300, 8756, 40956, 407, 1254, 437];
+let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+let date = new Date();
 
-arr.forEach(item => {
-    if (item.toString()[0] === '2' || item.toString()[0] === '4') {
-        console.log(item);
-    }
-})
+for (let i = 0, len = week.length; i < len; i++) {
+    week[i] = week[i].slice(0,1).toUpperCase().concat(week[i].slice(1));
+    let htmlItem = week[i];
+    if (i === (date.getDay()-1)) htmlItem = htmlItem.bold(); 
+    else if (i > 4) htmlItem = htmlItem.italics(); 
 
-
-// Задание 2. Решение с помощью функции и цикла
-
-function getPrimeDigits(num) {
-    for(let i = 1; i < num; i++) {
-        let isPrime = true;
-        for(let n = 2; n < i; n++) {
-            if (i % n === 0) isPrime = false;
-        }
-        if (isPrime) console.log(`Делители числа ${i}: 1 и ${i}`);
-    }
-}
-
-getPrimeDigits(100);
-
-
-//  Задание 2. Решение с помощью меток
-
-let num = 100;
-
-getNextNumber:
-for(let i = 1; i <= num; i++) {
-    if (i === 1) continue;
-
-    for(let n = 1; n < i; n++) {
-        if (n === 1) continue;
-        if (i % n === 0) continue getNextNumber;
-    }
-    console.log(`Делители числа ${i}: 1 и ${i}`);
+    const div = document.createElement('div');
+    div.innerHTML = htmlItem;
+    document.body.appendChild(div);
 }
