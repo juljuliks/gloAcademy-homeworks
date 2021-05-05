ecd2c8dd2edfa64c9e7744479e6d5975939931e9
Julia Glukhova <juliaglukhova666@gmail.com>
2021-05-05 12:52:30 +0300

    lesson28

--- hard-task/script.js
+++ hard-task/script.js
@@ -4,42 +4,46 @@ document.addEventListener('DOMContentLoaded', () => {
     const select = document.getElementById('cars'),
         output = document.getElementById('output');
 
-    const getData = () => {
-        return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.open('GET', './cars.json', true);
-            request.setRequestHeader('Content-type', 'application/json');
-            request.addEventListener('readystatechange', () => {
-                if (request.status === 200 || request.readyState === 4) {
-                    let data = JSON.parse(request.responseText)
-                    resolve(data)
-                }
-                else if (request.status === 200 && request.readyState !== 4) {
-                    let reason = request.readyState
-                    reject(reason)
-                }
-            });
-            request.send();
-        });
-    }
-
-    select.addEventListener('change', () => {
-        getData()
-            .then((data) => {
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const {
-                            brand,
-                            model,
-                            price
-                        } = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
-                    Цена: ${price}$`;
+        const getData = () => {
+            return new Promise((resolve, reject) => {
+                const request = new XMLHttpRequest();
+                request.addEventListener('readystatechange', () => {
+                    if (request.readyState !== 4) {
+                        return;
+                    }
+                    if (request.status === 200) {
+                        let data = JSON.parse(request.responseText)
+                        resolve(data)
+                    }
+                    else {
+                        let reason = request.readyState
+                        reject(reason)
                     }
                 });
-            }).catch((reason) => {
-                console.log(reason);
-                output.innerHTML = 'Произошла ошибка';
-            })
-    });
+                request.open('GET', './cars.json', true);
+                request.setRequestHeader('Content-type', 'application/json');
+                request.send();
+            });
+        }
+
+        select.addEventListener('change', () => {
+            getData()
+                .then((data) => {
+                    data.cars.forEach(item => {
+                        if (item.brand === select.value) {
+                            const {
+                                brand,
+                                model,
+                                price
+                            } = item;
+                            output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
+                        }
+                    });
+                }).catch((reason) => {
+                    console.log(reason);
+                    output.innerHTML = 'Произошла ошибка';
+                })
+        });
+
 });
