7ec4a2aece47badd550b12cfd02e855b069c6a54
Julia Glukhova <juliaglukhova666@gmail.com>
2021-05-06 20:08:22 +0300

    lesson30

---
+++ 3D-glo/webpack.dev.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'dev-bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    mode: 'development',
+    devServer: {
+        open: true,
+        port: 9000,
+        hot: true,
+        writeToDisk: false,
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                },
+            },
+            exclude: /node_modules/,
+        }]
+    }
+};
